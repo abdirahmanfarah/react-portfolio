38aa8ed9267ea0880cd355d8e8f68ddef04373ce
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-02-16 17:52:56 -0800

    Set up

---
+++ portfolio/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Abdirahman Farah</h1>
+    </div>
+  );
+}
+
+export default App;
