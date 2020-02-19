1b9caaafd7475ba6220c8dbbbc857ae4a2232432
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-02-18 19:44:03 -0800

    chose color palette

---
+++ portfolio/src/mui/custom-theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#722ABE"
+    },
+    secondary: {
+      main: "#77BE2A"
+    }
+  },
+  typography: {}
+});
